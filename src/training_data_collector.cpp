3346da9bde86e2d8f13e14aa3bc268ef0bbeffdc
Rocky14683 <rocky10208@gmail.com>
2025-04-05 14:36:54 -0400

    example

--- src/training_data_collector.cpp
+++ src/training_data_collector.cpp
@@ -4,22 +4,27 @@
 #include <iostream>
 #include <map>
 #include <string>
-
+#include <pcl/io/pcd_io.h>
 #include <cnpy.h>
+#include <vector>
+#include <ranges>
+#include "nn_helper.hpp"
 
-const int Nx = 128;
-const int Ny = 64;
-const int Nz = 32;
+
+std::vector<pcl::PointCloud<pcl::PointXYZI>> process_pcl_subroutine(size_t num_process_frame,
+                                                                    const std::filesystem::path& path) {
+    std::vector<pcl::PointCloud<pcl::PointXYZI>> datas(num_process_frame);
+    return {};
+}
+
 
 int main() {
-    // set random seed so that result is reproducible (for testing)
-    srand(0);
-    // create random data
-    std::vector<std::complex<double>> data(Nx * Ny * Nz);
-    for (int i = 0; i < Nx * Ny * Nz; i++) data[i] = std::complex<double>(rand(), rand());
-
-    // save it to file
-    cnpy::npy_save("../training_data/arr1.npz", &data[0], {Nz, Ny, Nx}, "wx");
-    std::println("Hello, World!");
+    InputFeatures features = {
+        {{1.f, 2.f, 3.f}, {2.f, 5.f, 1.f}, {7.f, 8.f, 9.f}},
+        Eigen::Quaternion<float> {0, 1, 0, 0}, // q, x, y, z
+        Label_classes::CYCLIST,
+    };
+    store_input_features(0, features);
+    auto out = parse_npz_features(0);
     return 0;
 }
