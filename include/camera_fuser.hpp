4e292c09053f131d56bc974268d3b81885d2c867
Rocky14683 <rocky10208@gmail.com>
2025-04-05 18:34:58 -0400

    move camera datas to camera_fuser.hpp

--- include/camera_fuser.hpp
+++ include/camera_fuser.hpp
@@ -33,8 +33,7 @@ std::vector<cv::Mat> load_images_from_dir_name(const std::filesystem::path& imag
 namespace calib_parser {
 using namespace Eigen;
 
-struct CalibConfig {
-        /**
+/**
 - S_xx: 1x2 size of image xx before rectification {w, h}
 - K_xx: 3x3 calibration matrix of camera xx before rectification
 - D_xx: 1x5 distortion vector of camera xx before rectification
@@ -46,21 +45,52 @@ struct CalibConfig {
 
 Note: When using this dataset you will most likely need to access only
 P_rect_xx, as this matrix is valid for the rectified image sequences.
-         https://www.cvlibs.net/publications/Geiger2013IJRR.pdf
-         https://docs.opencv.org/4.x/dd/d53/tutorial_py_depthmap.html
-         */
-        using S_MAT = Matrix<float, 1, 2>;
-        using K_MAT = Matrix<float, 3, 3>;
-        using D_MAT = Matrix<float, 1, 5>;
-        using R_MAT = Matrix<float, 3, 3>;
-        using T_MAT = Matrix<float, 3, 1>;
-        using S_RECT_MAT = Matrix<float, 1, 2>;
-        using R_RECT_MAT = Matrix<float, 3, 3>;
-        using P_RECT_MAT = Matrix<float, 3, 4>;
+*/
 
-        std::array<T_MAT, 2> T;
-        std::array<R_MAT, 2> R;
-        std::array<P_RECT_MAT, 4> R_rect;
-};
+using R_MAT = Eigen::Matrix<float, 3, 3>;
+using T_MAT = Eigen::Matrix<float, 3, 1>;
+using R_RECT_MAT = Eigen::Matrix<float, 3, 3>;
+
+// rotate -90deg on z axis
+const R_MAT
+    rotation_z_n45({{cosf(-M_PI_2), -sinf(-M_PI_2), 0.f}, {sinf(-M_PI_2), cosf(-M_PI_2), 0.f}, {0.f, 0.f, 1.f}});
+
+// rotate -90deg on x axis
+const R_MAT
+    rotation_x_n45({{1.f, 0.f, 0.f}, {0.f, cosf(-M_PI_2), -sinf(-M_PI_2)}, {0.f, sinf(-M_PI_2), cosf(-M_PI_2)}});
+
+const R_MAT cams_rotation = rotation_z_n45 * rotation_x_n45;
+
+// T_02: 5.956621e-02 2.900141e-04 2.577209e-03
+T_MAT T_02({{5.956621e-02}, {2.900141e-04}, {2.577209e-03}});
+
+// T_03: -4.731050e-01 5.551470e-03 -5.250882e-03
+T_MAT T_03({{-4.731050e-01}, {5.551470e-03}, {-5.250882e-03}});
+
+// R_02:
+// 9.999758e-01 -5.267463e-03 -4.552439e-03
+// 5.251945e-03 9.999804e-01 -3.413835e-03
+// 4.570332e-03 3.389843e-03 9.999838e-01
+R_MAT R_02({{9.999758e-01, -5.267463e-03, -4.552439e-03},
+            {5.251945e-03, 9.999804e-01, -3.413835e-03},
+            {4.570332e-03, 3.389843e-03, 9.999838e-01}});
+
+// R_03:
+// 9.995599e-01 1.699522e-02 -2.431313e-02
+// -1.704422e-02 9.998531e-01 -1.809756e-03
+//  2.427880e-02 2.223358e-03 9.997028e-01
+R_MAT R_03({{9.995599e-01, 1.699522e-02, -2.431313e-02},
+            {-1.704422e-02, 9.998531e-01, -1.809756e-03},
+            {2.427880e-02, 2.223358e-03, 9.997028e-01}});
+
+/*
+ * Correct camera transformation formula:
+ * X_i = (R_0i)^T * (X_0 - T_0i)
+ */
+inline Eigen::Matrix<float, 3, 1> transform_camera(const R_MAT& R_0i, const T_MAT& T_0i) {
+    static const Eigen::Matrix<float, 3, 1> X_0 = Eigen::Matrix<float, 3, 1>::Zero();
+    auto X_i = R_0i.transpose() * (X_0 - T_0i);
+    return X_i;
+}
 
 }; // namespace calib_parser
