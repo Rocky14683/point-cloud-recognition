d33968f7202e345cd94a2636b86b25806445edd7
Rocky14683 <rocky10208@gmail.com>
2025-04-19 13:47:25 -0400

    add mdspam flatten function

--- include/helper.hpp
+++ include/helper.hpp
@@ -30,5 +30,17 @@ namespace helper {
         return output_vector;
     }
 
+    std::vector<float> flatten_mdspan(const std::mdspan<const float, std::extents<size_t, 3, 32, 32, 32>>& m) {
+        std::vector<float> out;
+        out.reserve(3 * 32 * 32 * 32);
+
+        for (size_t c = 0; c < 3; ++c)
+            for (size_t d = 0; d < 32; ++d)
+                for (size_t h = 0; h < 32; ++h)
+                    for (size_t w = 0; w < 32; ++w)
+                        out.push_back(m[c, d, h, w]);
+
+        return out;
+    }
 
 };
